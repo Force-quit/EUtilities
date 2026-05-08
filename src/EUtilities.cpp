6ce638eea1fde48b4abb809ccc8fdc468111e76b
Force-quit <74936311+Force-quit@users.noreply.github.com>
2026-05-08 12:00:26 -0400

    Adding more tests (#12)
    * Adding more tests
    
    * Fixing the CI build

--- src/EUtilities.cpp
+++
@@ -1,2 +0,0 @@
-#include "EUtilities.hpp"
-
--- tests/EUtilitiesTest.cpp
+++ tests/EUtilitiesTest.cpp
@@ -8,13 +8,23 @@ TEST(EUtilities, SleepTest)
 {
 	using namespace std::chrono_literals;
 
-    std::clock_t start{ std::clock() };
+	auto start{ std::chrono::steady_clock::now() };
 	eutilities::sleepFor(1s);
-	std::clock_t end{ std::clock() };
-	EXPECT_GE(std::chrono::seconds(end - start), 1s);
+	auto end{ std::chrono::steady_clock::now() };
+	EXPECT_GE(std::chrono::duration_cast<std::chrono::seconds>(end - start), 1s);
 
-	start = std::clock();
+	start = std::chrono::steady_clock::now();
 	eutilities::sleepFor(250ms);
-	end = std::clock();
-	EXPECT_GE(std::chrono::milliseconds(end - start), 250ms);
+	end = std::chrono::steady_clock::now();
+	EXPECT_GE(std::chrono::duration_cast<std::chrono::milliseconds>(end - start), 250ms);
+
+	start = std::chrono::steady_clock::now();
+	eutilities::sleepFor(100us);
+	end = std::chrono::steady_clock::now();
+	EXPECT_GE(std::chrono::duration_cast<std::chrono::microseconds>(end - start), 100us);
+
+	start = std::chrono::steady_clock::now();
+	eutilities::sleepFor(100ns);
+	end = std::chrono::steady_clock::now();
+	EXPECT_GE(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start), 100ns);
 }
